3bd510b917c5f949ef8478f162408977305db868
Guido-PMC <101594840+Guido-PMC@users.noreply.github.com>
2022-04-16 16:02:00 -0300

    Add files via upload

---
+++ push_to_zabbix.py
@@ -0,0 +1,13 @@
+import requests
+import time
+time.sleep(60)
+url = "http://192.168.10.3:5000/wallet/0xe68702d72e47fd1ad5cbe5a0bc6911500b91eefd"
+
+payload={}
+headers = {
+  'api_key': '48d69e24-6723-484a-988b-be1da234848f'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
